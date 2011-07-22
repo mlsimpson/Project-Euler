bfec19b9616f81ffb2efb5f77eb4b0e2b54a023e
Matt Simpson <mr.threv@gmail.com>
2011-07-21 22:44:27 -0400

    Removed unnecessary code from 5.rb

--- 5.rb
+++ 5.rb
@@ -20,18 +20,6 @@ def lcm(x, y)
   (x*y) / gcd(x, y)
 end
 
-
-def sieve(x)
-  s = (0..x.to_i).to_a
-  s[0] = s[1] = nil
-  s.each{ |p| # Perform the block for each entry in the array.
-    next unless p # Iterate on next array value if entry is nil
-    break if p * p > x # Cease iteration if p*p > max_value
-    (p*p).step(x, p) { |m| s[m] = nil } # Starting at p*p, set every p-th value to "nil" until max_value
-  }
-  s.compact # Removes all nil entries and returns the compacted array.
-end
-
 # My personal method
 timing_method do
   result = 1
