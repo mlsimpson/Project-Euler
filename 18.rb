fd470f0788c928b3a5ae6a24041b8b580e2b4864
Matt Simpson <mr.threv@gmail.com>
2011-07-25 09:20:37 -0400

    Completed Problem 18

---
+++ 18.rb
@@ -0,0 +1,84 @@
+#!/usr/bin/env ruby
+
+require 'timing_method'
+
+# By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+#
+# 3
+# 7 4
+# 2 4 6
+# 8 5 9 3
+#
+# That is, 3 + 7 + 4 + 9 = 23.
+#
+# Find the maximum total from top to bottom of the triangle below:
+#
+# 75
+# 95 64
+# 17 47 82
+# 18 35 87 10
+# 20 04 82 47 65
+# 19 01 23 75 03 34
+# 88 02 77 73 07 63 67
+# 99 65 04 28 06 16 70 92
+# 41 41 26 56 83 40 80 70 33
+# 41 48 72 33 47 32 37 16 94 29
+# 53 71 44 65 25 43 91 52 97 51 14
+# 70 11 33 28 77 73 17 78 39 68 17 57
+# 91 71 52 38 17 14 91 43 58 50 27 29 48
+# 63 66 04 68 89 53 67 30 73 16 69 87 40 31
+# 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+#
+# NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route.
+# However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
+
+data1 = <<CUT
+3
+7 4
+2 4 6
+8 5 9 3
+CUT
+
+data = <<CUT
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+CUT
+
+timing_method do
+
+  # Separate each row into its own array of distinct numbers (currently strings)
+  rows = data.map{|str|
+    str.chomp.split.map{|x| x.to_i}
+  }
+
+  # puts [rows[0][0] + rows[1][0], rows[0][0] + rows[1][1]].max
+
+  # i = 0
+  # rows[rows.length - 1].inject(0){|max, value|
+  #   p "value = #{value}, rows[rows.length - 2][i] = #{rows[rows.length - 2][i]}, rows[rows.length - 2][i-1] = #{rows[rows.length - 2][i-1]}"
+  #   p [value+rows[rows.length - 2][i], value+rows[rows.length - 2][i-1]].max
+  #   p "i = #{i}"
+  #   i += 1
+  # }
+
+  (rows.length - 1).downto(0){|rownum|
+    0.upto(rownum - 1){|value|
+      rows[rownum - 1][value] += [rows[rownum][value], rows[rownum][value+1]].max
+    }
+  }
+
+  p rows[0][0]
+end
