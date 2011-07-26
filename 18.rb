b0f87c848494ee0005c19dae08e4888709269db7
Matt Simpson <mr.threv@gmail.com>
2011-07-26 00:32:11 -0400

    Edited Problems 18 & 67

--- 18.rb
+++ 18.rb
@@ -64,16 +64,6 @@ timing_method do
     str.chomp.split.map{|x| x.to_i}
   }
 
-  # puts [rows[0][0] + rows[1][0], rows[0][0] + rows[1][1]].max
-
-  # i = 0
-  # rows[rows.length - 1].inject(0){|max, value|
-  #   p "value = #{value}, rows[rows.length - 2][i] = #{rows[rows.length - 2][i]}, rows[rows.length - 2][i-1] = #{rows[rows.length - 2][i-1]}"
-  #   p [value+rows[rows.length - 2][i], value+rows[rows.length - 2][i-1]].max
-  #   p "i = #{i}"
-  #   i += 1
-  # }
-
   (rows.length - 1).downto(0){|rownum|
     0.upto(rownum - 1){|value|
       rows[rownum - 1][value] += [rows[rownum][value], rows[rownum][value+1]].max
